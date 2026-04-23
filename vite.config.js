dc24abfd0837d47bf0bdb928a53ffdcd415b33d4
lesythx <leslienpadua@gmail.com>
2026-04-24 03:26:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  publicDir: 'static',
-  plugins: [svelte()],
-  server: { port: 3000 }
-});
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
